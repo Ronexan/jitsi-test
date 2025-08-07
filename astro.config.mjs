7dc347eae8351153995d138f82a187b02072d3c6
David Eden <daveyedenex@gmail.com>
2025-08-07 13:05:23 +0100

    Switched to vercel adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [react()],
-  adapter: node({
-    mode: 'middleware'
-  })
+  adapter: vercel()
 });
